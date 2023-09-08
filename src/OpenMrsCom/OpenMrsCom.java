b850e4223d0995e92d3f1948aa0c75548b611ad2
DenizOzdem <denizozdem@gmail.com>
2023-09-08 20:19:58 +0300

    Master dosya oluşturuldu.

--- src/OpenMrsCom/OpenMrsCom.java
+++ src/OpenMrsCom/OpenMrsCom.java
@@ -1,10 +1,67 @@
 package OpenMrsCom;
 
 import Utility.BaseDriver;
+import org.testng.annotations.Test;
 
-public class OpenMrsCom extends BaseDriver{
+public class OpenMrsCom extends BaseDriver {
 
-    
+    @Test
+    public void SistemeGirisHatalariniKontrol() {
 
+    }
+
+
+    @Test
+    public void Login() {
+
+    }
+
+
+    @Test
+    public void Logout() {
+
+    }
+
+
+    @Test
+    public void HastaKaydi() {
+
+    }
+
+
+    @Test
+    public void MyAccount() {
+
+    }
+
+
+    @Test
+    public void HastaArama() {
+
+    }
+
+
+    @Test
+    public void HastaSilme() {
+
+    }
+
+
+    @Test
+    public void HastaListeleme() {
+
+    }
+
+
+    @Test
+    public void HastaKayitlariniBirlestirme() {
+
+    }
+
+
+    @Test
+    public void RandevuYanlisSaatDilimi() {
+
+    }
 
 }
