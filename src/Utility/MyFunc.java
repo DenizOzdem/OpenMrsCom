303a80333f461967f67fb3d1603a00843065b7ce
JavaAhmet <tefekahmet@gmail.com>
2023-09-13 20:19:19 +0300

    register completed

--- src/Utility/MyFunc.java
+++ src/Utility/MyFunc.java
@@ -6,10 +6,10 @@ import org.testng.Assert;
 
 public class MyFunc {
 
-    public static void Bekle(int sn){
+    public static void Bekle(int sn) {
 
         try {
-            Thread.sleep(sn*1000l);
+            Thread.sleep(sn * 1000l);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -22,5 +22,10 @@ public class MyFunc {
 //        WebElement msgLabel=BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
 //        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
 //    }
+    public static int randomGenerator(int sinir) {
+    return(int)(Math.random()*sinir);
+
+    }
+
 
 }
